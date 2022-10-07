e830c8c7ade85bd30d6fc9222d0ff99714d4e940
thalia-araujoo <115299056+thalia-araujoo@users.noreply.github.com>
2022-10-07 16:08:35 -0300

    Add files via upload

---
+++ pratic logica.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <locale.h>
+int main() {
+	setlocale(LC_ALL, "Portuquese_Brazil");
+	
+	float n1, n2, n3, media;
+	
+	printf("primeira nota:");
+	scanf("%f", &n1);
+	
+	printf("segunda nota:");
+	scanf("%f", &n2);
+	
+	printf("terceira nota:");
+	scanf("%f", &n3);
+	
+	media = (n1 + n2 +n3) /3;
+	
+	printf("O valor calculado entre os números %f, %F e %f foi:%f", n1, n2, n3, media);
+	
+	return 0;
+}
